e161a6506d80e263c1a3c37662380f677d348bff
chencong <sccong@outlook.com>
2023-04-10 10:25:03 +0800

    initial

---
+++ main/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      main
+      <micro-app
+        iframe
+        name="scc-child"
+        url="http://localhost:5173"
+      ></micro-app>
+    </div>
+  )
+}
+
+export default App
